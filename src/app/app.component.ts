7429ef27a6590209a8eea4e2bac35412192b18bd
Rostyslav Lekh <rostlex2001@gmail.com>
2020-08-25 18:49:55 +0300

    first lesson

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'users-list';
+  typesOfShoes: string[] = ['Smith', 'Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
+}
