4d209d565e8127aeb1edd71d34bd54086f4b03e3
Rostyslav Lekh <rostlex2001@gmail.com>
2020-08-27 14:44:21 +0300

    third lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Список пользователей';
-  typesOfShoes: string[] = ['Smith', 'Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
 }
